fa228067c32620395228094e2b96d24491d95117
prnvhub <74250536+prnvhub@users.noreply.github.com>
2021-12-09 16:33:48 +0530

    cleaning

--- sma.py
+++ sma.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import datetime as dt
 
-
+# Load the file
 data=pd.read_excel('RELIANCE.xlsx')
+
+# Formatting the date and removing time.
 data['Date']=pd.to_datetime(data['Date'], format='%Y-%m-%d').dt.date
+
+# SMA of 10 Days
 data['SMA(Open) 10D']=data['Open'].rolling(10, min_periods=5).mean()
 data['SMA(High) 10D']=data['High'].rolling(10, min_periods=9).mean()
 data['SMA(Low) 10D']=data['Low'].rolling(10, min_periods=9).mean()
